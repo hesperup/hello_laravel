b86da9557abad2cbdb792a736cb13725551f958d
hesper <hesperup@gmail.com>
2020-05-17 05:53:22 +0000

    add csrf

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -12,4 +13,17 @@ class UserController extends Controller
         return view('user.create');
     }
 
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
