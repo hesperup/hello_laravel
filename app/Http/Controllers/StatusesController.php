7827737ddd9dc548a9ec9caee5e4f3c5209b2c48
hesper <hesperup@gmail.com>
2020-05-18 05:39:48 +0000

    delete one weibo

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,13 +18,21 @@ class StatusesController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
-            'content'=>'required|max:140'
+        $this->validate($request, [
+            'content' => 'required|max:140'
         ]);
         Auth::user()->statuses()->create(
-          [  'content' => $request['content']]
+            ['content' => $request['content']]
         );
-        session()->flash('success','发布成功');
-        return redirect( )->back();
+        session()->flash('success', '发布成功');
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('delete', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
+        return redirect()->back();
     }
 }
